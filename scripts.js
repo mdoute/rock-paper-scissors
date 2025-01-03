fda4c167c2199c87853f0b1c5d406b147712e44b
Mike <mdoute@gmail.com>
2025-01-03 17:51:38 -0500

    Added base GUI

--- scripts.js
+++ scripts.js
@@ -1,97 +1,109 @@
+const rock = document.getElementById("rock");
+const paper = document.getElementById("paper");
+const scissors = document.getElementById("scissors");
+const playerSelection = document.getElementById("playerSelection");
+const computerSelection = document.getElementById("computerSelection");
+const roundOutcome = document.getElementById("roundOutcome");
+const gameScore = document.getElementById("gameScore");
+const computerScoreContainer = document.getElementById("computerScoreContainer");
+const playerScoreContainer = document.getElementById("playerScoreContainer");
+const roundDisplay = document.getElementById("roundDisplay");
+const buttonRow = document.getElementById("buttonRow");
+const newGameButtonRow = document.getElementById("newGameButtonRow");
+const newGameButton = document.createElement("button");
+newGameButton.addEventListener("click", playGame);
 
-// Get Computer choice
+
 function getComputerChoice() {
-
-    // Generate a random number between 0-0.99 with Math.random
-    // Multiply the random number by 3
-    // Round the random number down to the nearest whole number, either 0, 1, or 2
-
     var computerChoice = Math.floor(Math.random() * 3);
-
-    // Assign Rock, Paper, or Scissors based on the random number
-
     if (computerChoice === 0) {
-        //console.log("Computer chose: Rock"); // Log the computer's choice to make testing easier
         return "Rock";
     } else if (computerChoice === 1) {
-        //console.log("Computer chose: Paper"); // Log the computer's choice to make testing easier
         return "Paper";
     } else if (computerChoice === 2) {
-        //console.log("Computer chose: Scissors"); // Log the computer's choice to make testing easier
         return "Scissors";
     }
 }
 
-// Get Human choice
-function getHumanChoice() {
-
-    // Ask the human for their choice
-    // Convert their input to lowercase to match ALL InPuT cases
-    var humanChoice = prompt("Rock, paper, or scissors?", "Choose wisely...").toLowerCase();
-
-    // Convert human choice to a number
-
-    if (humanChoice === "rock") {
-        return "Rock";
-    } else if (humanChoice === "paper") {
-        return "Paper";
-    } else if (humanChoice === "scissors") {
-        return "Scissors";
-    }
+function resetGame() {
+    playerScoreContainer.innerText = "Player: 0";
+    computerScoreContainer.innerText = "Computer: 0";
+    roundDisplay.innerText = "Round 1 / 5";
+    roundOutcome.innerText = "";
+    welcomeMessage.innerText = "Welcome! Let's play again!";
+    buttonRow.setAttribute("style", "display:block;");
+    newGameButtonRow.innerHTML = "";
+    humanScore = 0;
+    computerScore = 0;
+    roundCount = 0;
 }
 
 function playGame() {
-    //Define player and computer score variables, set to 0
+    resetGame();
     let humanScore = 0;
     let computerScore = 0;
-
-    console.log("Welcome! This game consists of 5 rounds. The player the with highest score at the end wins. If you didn't assume that's how a winner was chosen, this may be a little too advanced for you. Check out Hello Kitty Island Adventure instead, available now on Nintendo Switch, PlayStation, PC, and Xbox.");
-
-    //Define round logic with the round function
-    function playRound() {
+    let roundCount = 0;
+    
+    function playRound(humanChoice) {
         const computerChoice = getComputerChoice();
-        const humanChoice = getHumanChoice();
+        console.log(humanChoice);
 
         if (humanChoice === computerChoice) {
-            console.log("Tie! You both selected " + humanChoice +".")
+            roundCount ++
+            roundOutcome.innerText = "Tie! You both selected " + humanChoice +".";
         } else if (
             (humanChoice === "Rock" && computerChoice ==="Scissors") ||
             (humanChoice === "Paper" && computerChoice ==="Rock") ||
             (humanChoice === "Scissors" && computerChoice ==="Paper")
         ) {
+            roundCount ++
             humanScore ++
-            console.log("You win! " + humanChoice + " beats " + computerChoice + ".")
+            roundOutcome.innerText = "You win! " + humanChoice + " beats " + computerChoice + ".";
         } else if (
             (computerChoice === "Rock" && humanChoice ==="Scissors") ||
             (computerChoice === "Paper" && humanChoice ==="Rock") ||
             (computerChoice === "Scissors" && humanChoice ==="Paper")
         ) {
+            roundCount ++
             computerScore ++
-            console.log("You Lose! " + humanChoice + " loses to " + computerChoice + ". Womp womp!")
+            roundOutcome.innerText = "You Lose! " + humanChoice + " loses to " + computerChoice + ". Womp womp!";
       }
-      console.log("Player score: " + humanScore);
-      console.log("Computer score: " + computerScore);
+      playerScoreContainer.innerText = "Player: " + humanScore;
+      computerScoreContainer.innerText = "Computer: " + computerScore;
+      roundDisplay.innerText = "Round " + (roundCount + 1) + " / 5";
+      welcomeMessage.innerText = "";
+      if (roundCount === 5) {
+        gameOver();
+      }
     }
-    
-    //Play round 5 times
-    playRound();
-    playRound();
-    playRound();
-    playRound();
-    playRound();
 
-    //Declare winner after 5 rounds
+    rock.addEventListener('click', function() {
+        playRound(rock.innerHTML);
+    });
+
+    paper.addEventListener('click', function() {
+        playRound(paper.innerHTML);
+    });
+
+    scissors.addEventListener('click', function() {
+        playRound(scissors.innerHTML);
+    });
+
     function gameOver() {
+        roundDisplay.innerText = "GAME OVER";
+        buttonRow.setAttribute("style", "display:none;");
+        newGameButtonRow.append(newGameButton);
+        newGameButton.setAttribute("style", "color: white; background: green;");
+        newGameButton.textContent = "New Game";
+
         if (humanScore > computerScore) {
-            console.log("You win! Congratulations, you're smarter than a Javascript robot made by me, a total beginner programmer with no experience. Be proud of yourself, but don't quit that day job just yet. You've got a lot of work to do if you want to win the big RPS competition in Capital City.")
+            welcomeMessage.innerText = "You win! Congratulations, you're smarter than a Javascript robot made by me, a total beginner programmer with no experience. Be proud of yourself, but don't quit that day job just yet. You've got a lot of work to do if you want to win the big RPS competition in Capital City.";
         } else if (humanScore < computerScore) {
-            console.log("You lose. All your rock paper scissors are belong to us.")
+            welcomeMessage.innerText = "You lose. All your rock paper scissors are belong to us.";
         } else if (humanScore === computerScore) {
-            console.log("You tried, but unfortunately you tied. Better luck next time!")
+            welcomeMessage.innerText = "You tried, but unfortunately you tied. Better luck next time!";
         }
     }
-
-    gameOver();
 
 }
 
